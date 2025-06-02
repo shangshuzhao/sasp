518db89d77e60e60be18dc569fea728f3b5be640
Shangshu <shangshuzhao@gmail.com>
2025-06-01 22:48:39 -0400

    more tests

--- GAE_train.py
+++ GAE_train.py
@@ -178,6 +178,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    for alpha in [i / 10 for i in range(2, 10)]:
-        main(seed = args.seed, alpha = alpha, device = device)
+    main(seed = args.seed, alpha = 0.99, device = device)
